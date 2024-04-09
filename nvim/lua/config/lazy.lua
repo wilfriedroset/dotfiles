8ceec580cfa879f8b38254faa29805e7d112487e
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2024-04-09 16:00:39 +0200

    update
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,12 +12,13 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.formatting.black" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
