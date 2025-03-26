d7a973369987534d4bc4810a895b55f25af7caeb
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2025-03-26 22:27:12 +0100

    [nvim] Replace telescope by fzf-lua
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -43,35 +43,9 @@ return {
   },
 
   {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-
-  -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
   },
 
   -- add more treesitter parsers
