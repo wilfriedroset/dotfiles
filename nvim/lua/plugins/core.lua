a7b9df9e26c9cc82838b8e63e29d12c8822c7dfc
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2025-03-26 22:23:12 +0100

    [nvim] Remove simrat39/symbols-outline.nvim as it is unmaintained
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -42,19 +42,6 @@ return {
     opts = {},
   },
 
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = {
-      { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" },
-    },
-    config = true,
-    opts = {
-      autofold_depth = 2,
-    },
-  },
-
   {
     "nvim-telescope/telescope.nvim",
     keys = {
