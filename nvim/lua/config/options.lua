5c47de2116b82374bee0fa03bc532f754f739ffe
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2025-08-08 22:36:39 +0200

    [nvim] Add initial setup for codecompanion
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,9 @@ vim.opt.clipboard = "" -- do not sync with system clipboard
 vim.opt.writebackup = false
 vim.opt.wrap = true
 vim.g.lazyvim_picker = "snacks"
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanionChat]])
---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest",
+    config = function()
+      require("mcphub").setup({
+        log = {
+          level = vim.log.levels.WARN,
+        },
+      })
+    end,
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
+    },
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
+      strategies = {
+        chat = { adapter = "ovhcloud" },
+        inline = { adapter = "ovhcloud" },
+        cmd = { adapter = "ovhcloud" },
+      },
+      adapters = {
+        acp = { show_defaults = false },
+        http = {
+          opts = { show_defaults = false, show_model_choices = true },
+          ovhcloud = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = {
+                url = "OPENAI_BASE_URL", -- envvar
+                api_key = "OPENAI_API_KEY", -- envvar
+                chat_url = "/chat/completions",
+                models_endpoint = "/models",
+              },
+              schema = {
+                model = {
+                  default = "gpt-oss-120b",
+                },
+              },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
